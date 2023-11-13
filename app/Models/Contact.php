e159c91d88880e7ce8ff122658f264a4ccdcf4bc
unknown <lemanhlinh209@gmail.com>
2023-11-13 13:47:01 +0700

    fix a

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     protected $guarded = ['id'];
+    protected $dates = ['created_time', 'edited_time'];
+
+    const CREATED_AT = 'created_time';
+    const UPDATED_AT = 'edited_time';
+
+    protected $table = 'contact';
 }
