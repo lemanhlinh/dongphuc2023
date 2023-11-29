cdb7a92f6516fcd5f9597c0cde0a96de7240812f
unknown <lemanhlinh209@gmail.com>
2023-11-29 17:33:17 +0700

    đs

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Role\CreateRole;
+use App\Http\Requests\Role\UpdateRole;
 use Illuminate\Http\Request;
 use App\DataTables\RoleDataTable;
 use App\Repositories\Contracts\RoleInterface;
 use App\Repositories\Contracts\PermissionInterface;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class RoleController extends Controller
 {
@@ -53,9 +57,29 @@ class RoleController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateRole $req)
     {
-        //
+        DB::beginTransaction();
+        try {
+            $role = $role = $this->roleRepository->create([
+                'name' => \Str::slug($req->display_name, '-'),
+                'display_name' => $req->display_name,
+            ]);
+            $this->roleRepository->updatePermission($role->id, $req->permissions);
+
+            DB::commit();
+            Session::flash('success', trans('message.create_role_success'));
+            return redirect()->back();
+        } catch (\Exception $exception) {
+            \Log::error([
+                'method' => __METHOD__,
+                'line' => __LINE__,
+                'context' => $exception->getMessage()
+            ]);
+            DB::rollBack();
+            Session::flash('danger', trans('message.create_role_error'));
+            return back();
+        }
     }
 
     /**
@@ -89,9 +113,29 @@ class RoleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update($id, UpdateRole $request)
     {
-        //
+        DB::beginTransaction();
+        try {
+            $this->roleRepository->update($id, [
+                'name' => \Str::slug($request->display_name, '-'),
+                'display_name' => $request->display_name,
+            ]);
+            $this->roleRepository->updatePermission($id, $request->permissions);
+
+            DB::commit();
+            Session::flash('success', trans('message.update_role_success'));
+            return redirect()->back();
+        } catch (\Exception $exception) {
+            \Log::error([
+                'method' => __METHOD__,
+                'line' => __LINE__,
+                'context' => $exception->getMessage()
+            ]);
+            DB::rollBack();
+            Session::flash('danger', trans('message.update_role_error'));
+            return back();
+        }
     }
 
     /**
@@ -102,6 +146,11 @@ class RoleController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $this->roleRepository->delete($id);
+
+        return [
+            'status' => true,
+            'message' => trans('message.delete_role_success')
+        ];
     }
 }
