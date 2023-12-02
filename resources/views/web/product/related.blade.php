6c103bbd4bfdde9997cdefcfbbba5093eb68588b
unknown <lemanhlinh209@gmail.com>
2023-12-02 10:49:07 +0700

    fsdfds

--- resources/views/web/product/related.blade.php
+++ resources/views/web/product/related.blade.php
@@ -11,12 +11,12 @@
         @forelse($product_related as $item)
         <div class="image-check">
             <figure class="imghvr-flip-vert">
-                <img src="{{ asset(str_replace('original', 'resized', $item->image)) }}" class="img-fluid" >
+                <img src="{{ asset(replace_image_to_webp($item->image)) }}" class="img-fluid" >
                 <figcaption>
                     @if($item->image_after)
-                    <img src="{{ asset(str_replace('original', 'resized', $item->image_after)) }}" alt="{{ $item->name }}" class="img-fluid" >
+                    <img src="{{ asset(replace_image_to_webp($item->image_after)) }}" alt="{{ $item->name }}" class="img-fluid" >
                     @else
-                    <img src="{{ asset(str_replace('original', 'resized', $item->image)) }}" class="img-fluid">
+                    <img src="{{ asset(replace_image_to_webp($item->image)) }}" class="img-fluid">
                     @endif
                 </figcaption><a href="{{ route('detailProduct', ['cat_slug' => $cat_slug, 'slug' => $item->alias])  }}"></a>
             </figure>
