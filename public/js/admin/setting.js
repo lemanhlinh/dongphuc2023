6f1de3d4b43b39073ec886f1a0b1c6b7c332c597
unknown <lemanhlinh209@gmail.com>
2024-04-16 13:59:55 +0700

    ffffgf

--- public/js/admin/setting.js
+++ public/js/admin/setting.js
@@ -1,2 +1,32 @@
-$(document).ready((function(){$("#change_lang").on("change",(function(){window.location=$(this).val()}))}));
-//# sourceMappingURL=setting.js.map
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/js/admin/setting.js":
+/*!***************************************!*\
+  !*** ./resources/js/admin/setting.js ***!
+  \***************************************/
+/***/ (() => {
+
+eval("$(document).ready(function () {\n  $('#change_lang').on('change', function () {\n    window.location = $(this).val();\n  });\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6WyIkIiwiZG9jdW1lbnQiLCJyZWFkeSIsIm9uIiwid2luZG93IiwibG9jYXRpb24iLCJ2YWwiXSwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vcmVzb3VyY2VzL2pzL2FkbWluL3NldHRpbmcuanM/N2JkYSJdLCJzb3VyY2VzQ29udGVudCI6WyIkKGRvY3VtZW50KS5yZWFkeShmdW5jdGlvbigpIHtcbiAgICAkKCcjY2hhbmdlX2xhbmcnKS5vbignY2hhbmdlJywgZnVuY3Rpb24gKCkge1xuICAgICAgICB3aW5kb3cubG9jYXRpb24gPSAgJCh0aGlzKS52YWwoKTtcbiAgICB9KVxufSk7XG4iXSwibWFwcGluZ3MiOiJBQUFBQSxDQUFDLENBQUNDLFFBQVEsQ0FBQyxDQUFDQyxLQUFLLENBQUMsWUFBVztFQUN6QkYsQ0FBQyxDQUFDLGNBQWMsQ0FBQyxDQUFDRyxFQUFFLENBQUMsUUFBUSxFQUFFLFlBQVk7SUFDdkNDLE1BQU0sQ0FBQ0MsUUFBUSxHQUFJTCxDQUFDLENBQUMsSUFBSSxDQUFDLENBQUNNLEdBQUcsQ0FBQyxDQUFDO0VBQ3BDLENBQUMsQ0FBQztBQUNOLENBQUMsQ0FBQyIsImZpbGUiOiIuL3Jlc291cmNlcy9qcy9hZG1pbi9zZXR0aW5nLmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./resources/js/admin/setting.js\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./resources/js/admin/setting.js"]();
+/******/ 	
+/******/ })()
+;
